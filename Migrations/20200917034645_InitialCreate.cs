1c95cc81812d34f64da76f5ba1800e65b74e8b41
tksatc <tksatc@gmail.com>
2021-10-05 07:40:28 -0800

    Add project files.

---
+++ Migrations/20200917034645_InitialCreate.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ToDoList.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GetItDone",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Description = table.Column<string>(nullable: false),
+                    CreatedDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GetItDone", x => x.ID);
+                });
+
+            migrationBuilder.InsertData(
+                table: "GetItDone",
+                columns: new[] { "ID", "CreatedDate", "Description" },
+                values: new object[] { 1, new DateTime(2020, 9, 16, 0, 0, 0, 0, DateTimeKind.Local), "Do CIT228 Homework" });
+
+            migrationBuilder.InsertData(
+                table: "GetItDone",
+                columns: new[] { "ID", "CreatedDate", "Description" },
+                values: new object[] { 2, new DateTime(2020, 9, 16, 0, 0, 0, 0, DateTimeKind.Local), "Take a nap" });
+
+            migrationBuilder.InsertData(
+                table: "GetItDone",
+                columns: new[] { "ID", "CreatedDate", "Description" },
+                values: new object[] { 3, new DateTime(2020, 9, 16, 0, 0, 0, 0, DateTimeKind.Local), "Work Out" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GetItDone");
+        }
+    }
+}
---
+++ Models/TheList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Models
+{
+    public class TheList
+    {
+        public int ID { get; set; }
+        [Required(ErrorMessage ="Please enter a task description.")]
+        public string Description { get; set; }
+
+        [Display(Name = "Created Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime CreatedDate { get; set; }
+    }
+}
